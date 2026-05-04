78da073cd837b500bf4aedac60455537877288bc
Brendon Butler <brendonaaron97@gmail.com>
2026-05-04 18:13:58 -0400

    add enemies to a group and destroy them and the bullet when collision is detected

--- bullet.gd
+++ bullet.gd
@@ -4,6 +4,14 @@ extends Area2D
 
 var direction = Vector2.ZERO
 
+func _ready() -> void:
+	body_entered.connect(_on_body_entered)
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.is_in_group("enemies"):
+		body.queue_free()
+		queue_free()
+
 func _physics_process(delta: float) -> void:
 	position += direction * speed * delta
 	
